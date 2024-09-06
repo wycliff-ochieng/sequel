deeb10035f1ee951747b0960ceb0c6d7c0bd3b65
wycliff <wyckieochieng91@gmail.com>
2024-09-06 08:53:43 -0800

    first

--- canada.sql
+++ canada.sql
@@ -2,7 +2,7 @@ create database Canada;
 
 use canada;
 
-create table country(
+create table province(
     province_id int,
     province_name varchar(100),
     official_language varchar(100)
@@ -46,18 +46,47 @@ values('1','Toronto','7'),
       ('10', 'St. Johns', '5');
 
 insert into tourist_attraction(attraction_id,attraction_name,attraction_city_id,open_flag)
-values('1 ','CN ','Tower' ,'1' true),
-      ('2','Old Quebec','2','true'),
-      ('3',,'Royal Ontario Museum''1', 'true'),
-      ('4', 'Place Royale', '2', 'true'),
-      ('5', 'Halifax Citadel', '3', 'true'),
-      ('6', 'Garrison District', '4', 'true'),
-      ('7','Confederation Centre of...','7', 'true'),
-      ('8', 'Stone Hall Castle', '8', 'true'),
-      ('9', 'West Edmonton Mall', '9', 'true'),
-      ('10','Signal Hill', '10',' true'),
-      ('11','Canadian Museum for Human...', '5',' true'),
-      ('12', 'Royal BC Museum', '6', 'true'),
-      ('13', 'Sunnyside Amusement Park', 'false');
+values('1 ','CN ','Tower' ,'1' ,True),
+      ('2','Old Quebec','2',True),
+      ('3',,'Royal Ontario Museum''1', True),
+      ('4', 'Place Royale', '2', True),
+      ('5', 'Halifax Citadel', '3', True),
+      ('6', 'Garrison District', '4', True),
+      ('7','Confederation Centre of...','7', True),
+      ('8', 'Stone Hall Castle', '8', True),
+      ('9', 'West Edmonton Mall', '9', True),
+      ('10','Signal Hill', '10',True),
+      ('11','Canadian Museum for Human...', '5',True),
+      ('12', 'Royal BC Museum', '6', True),
+      ('13', 'Sunnyside Amusement Park', False);
 
-select attraction_name from tourist_attraction
+--Write a query that performs an inner join between the three tables. Select the attraction_name column
+--from the tourist_attraction table, the city_name column from the capital_city table, and the province_name
+--column from the province table.
+select ta.attraction_name, cc.city_name, p.province_name
+
+from tourist_attraction ta
+
+inner join capital_city cc
+
+on cc.city_id = ta.attraction_city_id
+
+inner join province p
+
+on p.province_id = cc.province_id;
+
+--Select rows from the attraction table only where the open_flag is set to true. Select rows from the
+--province table only where the official_language is set to French.
+
+select * from tourist_attraction
+where open_flag is True;
+
+select * from province
+where official_language = 'French';
+
+--Create a temporary table called open_tourist_attraction that selects the attraction_city_id and
+--attraction_name columns from the tourist_attraction table where the open_flag value is true.
+
+create temporary table open_tourist_attraction
+
+
